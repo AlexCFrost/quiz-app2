92444e67e81f05f1fecbe7da7717dabaf7fa4fef
AlexCFrost <siva.ganesh.c18@gamil.com>
2023-12-23 14:19:41 +0530

    finished

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Quiz from './Components/Quiz'
+import './App.css'
+import { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Quiz/>
+      </div>
+    )
+  }
+}
